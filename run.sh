4a44c1ec62403d6b9c75fe86a33917b8118a4c9f
james-hansel <hanseljames10@gmail.com>
2025-01-16 16:06:01 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-# Download and install fnm:
-curl -o- https://fnm.vercel.app/install | bash
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 # Download and install Node.js:
-fnm install 22
+nvm install 22
 
 # Verify the Node.js version:
 node -v # Should print "v22.13.0".
+nvm current # Should print "v22.13.0".
 
 # Verify npm version:
 npm -v # Should print "10.9.2".
